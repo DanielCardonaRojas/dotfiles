374e17effee929a88c271a9c67171fd91013f09a
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2021-12-04 17:25:07 -0500

    Adjust scrollview

--- .config/nvim/lua/custom/autocmds.lua
+++ .config/nvim/lua/custom/autocmds.lua
@@ -1,5 +1,5 @@
 vim.cmd [[ autocmd BufEnter *.dart :lua vim.api.nvim_buf_set_option(0, "commentstring", "// %s") ]]
 vim.cmd [[ autocmd CursorHold * :lua vim.lsp.diagnostic.show_line_diagnostics() ]]
 -- vim.cmd [[ autocmd CursorHold * :lua require"gitsigns".blame_line() ]]
-
+-- vim.cmd [[ autocmd BufEnter,FileType *.dart set colorcolumn=81]]
 
--- .config/nvim/lua/plugins/configs/nvimtree.lua
+++ .config/nvim/lua/plugins/configs/nvimtree.lua
@@ -49,7 +49,7 @@ g.nvim_tree_icons = {
       default = "",
       empty = "", -- 
       empty_open = "",
-      open = "",
+      open = "",
       symlink = "",
       symlink_open = "",
    },
