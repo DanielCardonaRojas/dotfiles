5f3ced26ec60859400ea957bbd458ca512fb5cc0
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2021-12-06 23:50:20 -0500

    Format on save lsp

--- .config/nvim/lua/custom/autocmds.lua
+++ .config/nvim/lua/custom/autocmds.lua
@@ -1,5 +1,6 @@
 vim.cmd [[ autocmd BufEnter *.dart :lua vim.api.nvim_buf_set_option(0, "commentstring", "// %s") ]]
 vim.cmd [[ autocmd CursorHold * :lua vim.lsp.diagnostic.show_line_diagnostics() ]]
+vim.cmd [[ autocmd BufWritePre *.dart lua vim.lsp.buf.formatting() ]]
 -- vim.cmd [[ autocmd CursorHold * :lua require"gitsigns".blame_line() ]]
 -- vim.cmd [[ autocmd BufEnter,FileType *.dart set colorcolumn=81]]
 
