3596a0498b7eb446d373d28d7cfad25d0cc5dfc8
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2021-12-07 00:21:26 -0500

    Fix dashboard displayed mappings

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -92,6 +92,12 @@ hooks.add("install_plugins", function(use)
     "folke/which-key.nvim",
     config = function()
       require("which-key").setup {
+        layout = {
+          height = { min = 4, max = 12 }, -- min and max height of the columns
+          width = { min = 20, max = 50 }, -- min and max width of the columns
+          spacing = 8, -- spacing between columns
+          align = "center", -- align columns left, center or right
+        },
       }
     end
   }
