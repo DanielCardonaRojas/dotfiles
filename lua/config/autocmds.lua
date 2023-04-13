9556b3c51c9890d7b97afea1fb0f5922dced23b4
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-04-13 17:48:09 -0500

    disable relative number

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,3 +2,4 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.cmd([[ autocmd CursorHold * :lua vim.diagnostic.open_float() ]])
+vim.cmd([[ autocmd BufEnter *.dart :lua vim.api.nvim_buf_set_option(0, "commentstring", "// %s") ]])
