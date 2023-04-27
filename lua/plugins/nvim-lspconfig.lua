da384229a6dffde473fa89e9064bf54c0bfbaa8c
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-04-27 13:19:20 -0500

    add leader . keybinding for lsp code action

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,10 @@
 return {
   "neovim/nvim-lspconfig",
+  init = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] =
+      { "<leader>.", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v" }, has = "codeAction" }
+  end,
   opts = {
     diagnostics = {
       underline = true,
