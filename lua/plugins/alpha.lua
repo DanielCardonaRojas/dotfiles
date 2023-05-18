51f2753f9827d8eee45eff23ec4443c2bd602a49
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-05-17 20:53:48 -0500

    add obsidian plugin

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -12,6 +12,16 @@ return {
 
     local dashboard = require("alpha.themes.dashboard")
     dashboard.section.header.val = vim.split(logo, "\n")
+
+    local noteOpenButton = dashboard.button("m", " " .. " Open note", ":ObsidianQuickSwitch <CR>")
+    noteOpenButton.opts.hl = "AlphaButtons"
+    noteOpenButton.opts.hl_shortcut = "AlphaShortcut"
+
+    local newNoteButton = dashboard.button("e", "󰏫 " .. " Create note", ":ObsidianNew ", { silent = false })
+    newNoteButton.opts.hl = "AlphaButtons"
+    newNoteButton.opts.hl_shortcut = "AlphaShortcut"
+    table.insert(dashboard.section.buttons.val, 4, noteOpenButton)
+    table.insert(dashboard.section.buttons.val, 5, newNoteButton)
     return dashboard
   end,
 }
