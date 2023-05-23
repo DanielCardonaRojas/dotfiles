626480cfcac188302314d0b45617dfa15211e065
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2023-05-22 21:43:16 -0500

    add bind to toggle blame for current line

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  keys = {
+    {
+      "<leader>gb",
+      "<cmd>Gitsigns toggle_current_line_blame<CR>",
+      desc = "Toggle blame for current line",
+    },
+  },
+  opts = {},
+}
