f58ff97863129e8591e2019e1229b4752b004ae1
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-07-07 14:42:57 -0500

    add yabai script to go to next window across multiple spaces

---
+++ .config/scripts/nextWindow.sh
@@ -0,0 +1,15 @@
+CURRENT_WINDOW_INFO=$(yabai -m query --windows --window)
+CURRENT_WIN_ID=$(echo $CURRENT_WINDOW_INFO | jq '{id: .id, app: .app } | .id')
+CURRENT_WIN_NAME=$(echo $CURRENT_WINDOW_INFO | jq '{id: .id, app: .app } | .app')
+ALL_APPLICATION_WINDOW_IDS_EXPRESSION="map({app, id }) | group_by(.app) | map({app: .[0].app, ids: map(.id)}) | map(select(.app == $CURRENT_WIN_NAME))[0] | .ids"
+ALL_APPLICATION_WINDOW_IDS=$(yabai -m query --windows | jq "$ALL_APPLICATION_WINDOW_IDS_EXPRESSION")
+GET_NEXT_ID=".[index($CURRENT_WIN_ID) + 1]"
+NEXT_ID=$(echo $ALL_APPLICATION_WINDOW_IDS | jq "$GET_NEXT_ID")
+echo "app: $CURRENT_WIN_NAME ids: $ALL_APPLICATION_WINDOW_IDS"
+
+if [[ $NEXT_ID != "null" ]]; then
+	echo "moving to window with id: $NEXT_ID"
+	yabai -m window --focus $NEXT_ID
+else
+	echo "could not find another instance of: $CURRENT_WIN_NAME"
+fi
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,13 +1,31 @@
 hs.loadSpoon("RecursiveBinder")
+hs.loadSpoon("hs_select_window")
 
 spoon.RecursiveBinder.escapeKey = { {}, "escape" } -- Press escape to abort
 
 local singleKey = spoon.RecursiveBinder.singleKey
 
+local function nextWindowForApp()
+	-- local tildeKeyCode = 50
+	local hyper = { "shift", "ctrl", "cmd", "alt" }
+	hs.eventtap.keyStroke(hyper, "p")
+	-- local result = hs.execute("~/.config/scripts/nextWindow.sh")
+end
+
+local function launchFocusOrSwitch(name)
+	local currentApplication = hs.application.frontmostApplication():title()
+	if currentApplication == name then
+		nextWindowForApp()
+		return true
+	else
+		return hs.application.launchOrFocus(name)
+	end
+end
+
 local function openOneOf(names)
 	return function()
 		for _, name in pairs(names) do
-			if hs.application.launchOrFocus(name) then
+			if launchFocusOrSwitch(name) then
 				return
 			end
 		end
@@ -16,19 +34,22 @@ end
 
 local keyMap = {
 	[singleKey("x", "xcode")] = function()
-		hs.application.launchOrFocus("Xcode")
+		launchFocusOrSwitch("Xcode")
 	end,
 	[singleKey("s", "slack")] = function()
-		hs.application.launchOrFocus("Slack")
-	end,
-	[singleKey("t", "terminal")] = function()
-		hs.application.launchOrFocus("Alacritty")
-	end,
-	[singleKey("z", "zoom")] = function()
-		hs.application.launchOrFocus("zoom.us")
+		launchFocusOrSwitch("Slack")
 	end,
 	[singleKey("g", "git")] = openOneOf({ "Fork", "SourceTree" }),
-	[singleKey("b", "browser")] = openOneOf({ "Arc", "Google Chrome", "Safari" }),
+	[singleKey("f", "finder")] = function()
+		launchFocusOrSwitch("Finder")
+	end,
+	[singleKey("t", "terminal")] = function()
+		launchFocusOrSwitch("Alacritty")
+	end,
+	[singleKey("z", "zoom")] = function()
+		launchFocusOrSwitch("zoom.us")
+	end,
+	[singleKey("b", "browser")] = openOneOf({ "Arc", "Chrome", "Google Chrome", "Safari" }),
 	[singleKey("d", "domain+")] = {
 		[singleKey("g", "github")] = function()
 			hs.urlevent.openURL("https://github.com")
