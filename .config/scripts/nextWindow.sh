1c72440fb84ad61274db6a07083fb8df4188e1b2
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-07-12 11:41:26 -0500

    fix yabai script to cycle through application windows

--- .config/scripts/nextWindow.sh
+++ .config/scripts/nextWindow.sh
@@ -1,9 +1,9 @@
 CURRENT_WINDOW_INFO=$(yabai -m query --windows --window)
 CURRENT_WIN_ID=$(echo $CURRENT_WINDOW_INFO | jq '{id: .id, app: .app } | .id')
 CURRENT_WIN_NAME=$(echo $CURRENT_WINDOW_INFO | jq '{id: .id, app: .app } | .app')
-ALL_APPLICATION_WINDOW_IDS_EXPRESSION="map({app, id }) | group_by(.app) | map({app: .[0].app, ids: map(.id)}) | map(select(.app == $CURRENT_WIN_NAME))[0] | .ids"
+ALL_APPLICATION_WINDOW_IDS_EXPRESSION="map({app, id }) | group_by(.app) | map({app: .[0].app, ids: map(.id)}) | map(select(.app == $CURRENT_WIN_NAME))[0] | .ids | sort"
 ALL_APPLICATION_WINDOW_IDS=$(yabai -m query --windows | jq "$ALL_APPLICATION_WINDOW_IDS_EXPRESSION")
-GET_NEXT_ID=".[index($CURRENT_WIN_ID) + 1]"
+GET_NEXT_ID=".[(index($CURRENT_WIN_ID) + 1) % length]"
 NEXT_ID=$(echo $ALL_APPLICATION_WINDOW_IDS | jq "$GET_NEXT_ID")
 echo "app: $CURRENT_WIN_NAME ids: $ALL_APPLICATION_WINDOW_IDS"
 
