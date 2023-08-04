405e14e0cbe0111080c09f316a6bc53e81e032b6
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-08-04 09:59:32 -0500

    simplify password menu

--- .config/scripts/pass-fzf.sh
+++ .config/scripts/pass-fzf.sh
@@ -7,9 +7,7 @@ export FZF_DEFAULT_OPTS=" \
 --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
 --color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
 
-selected_file=$(find ~/.password-store/ -name "*.gpg" |
-	sed -r 's/.*\.password-store\/(.*)\.gpg/\1/g' |
-	fzf +m)
+selected_file=$(gopass list -f | fzf +m)
 
 if ! [ -z $selected_file ]; then
 	pass -c $selected_file
